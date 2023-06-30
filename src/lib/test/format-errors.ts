1252ebdb357b4e6e3a5e69edee3f02adf3f8ae96
Matt Sutkowski <msutkowski@gmail.com>
2023-06-29 21:02:38 -0700

    feat: support overriding default zod error handling (#8)

---
+++ src/lib/test/format-errors.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+
+export const error = z.object({
+  title: z.string(),
+  detail: z.string().nullish(),
+  source: z.unknown(),
+});
+
+export const validationError = z.object({
+  title: z.string(),
+  detail: z.string().nullish(),
+  source: z.object({
+    pointer: z.string(),
+  }),
+});
+
+export type ValidationError = z.infer<typeof validationError>;
+
+export const errorFormat = z.object({
+  errors: z.array(z.union([error, validationError])),
+});
+
+export type ErrorFormat = z.infer<typeof errorFormat>;
+
+export function formatZodErrors(zodError: z.ZodError): ErrorFormat {
+  const errors = zodError.errors.map((issue): ValidationError => {
+    return {
+      title: issue.message,
+      detail: issue.code,
+      source: {
+        pointer: `/${issue.path.join("/")}`,
+      },
+    };
+  });
+
+  return {
+    errors,
+  };
+}
