7c3ea5b21a0aea23765feaa0d672a6c6b1cc158b
Matt Sutkowski <msutkowski@gmail.com>
2023-10-08 11:08:14 -0400

    chore(perf): make flattenToNested faster

--- src/lib/flatten-to-nested.ts
+++ src/lib/flatten-to-nested.ts
@@ -1,19 +1,25 @@
 export function flattenToNested(obj: Record<string, any>): any {
   const result: any = {};
 
-  for (const [key, value] of Object.entries(obj)) {
-    const parts = key.split(/\]\[|\[|\]/).filter((p) => p.length);
+  const entries = Object.entries(obj);
+
+  for (let e = 0; e < entries.length; e++) {
+    const [key, value] = entries[e];
+
+    const parts = key.split(/\]\[|\[|\]/).filter(Boolean);
+
     let temp: any = result;
 
-    for (let i = 0; i < parts.length; i++) {
+    for (let i = 0, len = parts.length; i < len; i++) {
       const part = parts[i];
 
-      if (i === parts.length - 1) {
+      if (i === len - 1) {
         temp[part] = value;
       } else {
         if (!temp[part]) {
-          temp[part] = parts[i + 1].match(/^\d+$/) ? [] : {};
+          temp[part] = /^\d+$/.test(parts[i + 1]) ? [] : {};
         }
+
         temp = temp[part];
       }
     }
